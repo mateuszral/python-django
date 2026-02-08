867b359a13587f69e2261f9ade80c83d080e2f80
mateuszral <mral16102000@gmail.com>
2026-02-08 10:16:11 +0100

    add question and some choices

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,22 @@
+from datetime import timedelta
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
+    def __str__(self):
+        return f"{self.id}. {self.question_text} - {self.pub_date}"
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days = 1)
+    
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField("published date")
     
 class Choice(models.Model):
+    def __str__(self):
+        return f"{self.choice_text} - {self.votes} votes"
+    
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length = 200)
-    votels = models.IntegerFields(default = 0)
+    votes = models.IntegerField(default = 0)

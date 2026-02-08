f35f0a1669ffd0412c3cb220312bf75f1df0e101
mateuszral <mral16102000@gmail.com>
2026-02-08 11:20:29 +0100

    add template for displaying questions with links to them

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import HttpResponse
+from django.template import loader
+
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse("Hello world - polls")
+    questions_list = Question.objects.all()
+    template = loader.get_template('polls/index.html')
+    
+    res = template.render({'questions_list': questions_list})
+    return HttpResponse(res)   
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    questions = Question.objects.all()
+    res = ", ".join([q.question_text for q in questions])
+    return HttpResponse(res)    
 
 def results(request, question_id):
     return HttpResponse(f"You're looking at the results of question {question_id}.")
